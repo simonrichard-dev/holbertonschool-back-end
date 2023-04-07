a4b6934a3c4bb508431821c2355a2cb9a623a0ed
simonrichard-dev <simon.insa@gmail.com>
2023-04-07 18:33:03 +0200

    add 3-dictionary_of_list_of_dictionaries.py

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Dictionary of list of dictionaries """
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    response_users = requests.get('https://jsonplaceholder.typicode.com/users')
+    response_todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+
+    employees = response_users.json()
+    todos = response_todos.json()
+    username = ''
+    status = ''
+    task_title = ''
+    user_dict = {}
+    tasks_list = []
+
+    for i in employees:
+        username = i.get('username')
+        id = i.get('id')
+
+        for j in todos:
+            if id == j.get('userId'):
+                tasks_dict = {}
+                tasks_title = j.get('title')
+                status = j.get('completed')
+                tasks_dict["task"] = j.get('title')
+                tasks_dict["completed"] = status
+                tasks_dict["username"] = username
+                tasks_list.append(tasks_dict)
+        user_dict[id] = tasks_list
+
+    with open('todo_all_employees.json', 'w') as jsonfile:
+        json.dump(user_dict, jsonfile)
